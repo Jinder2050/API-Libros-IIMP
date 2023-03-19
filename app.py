367e20b998e6fed1d10013eed54991284ee124f3
Jinder2050 <yindercondori@gmail.com>
2023-03-19 17:08:27 -0500

    License and fix the bug

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
+from models import db, Libros
 
+# file_path = os.path.abspath(os.getcwd())+"\libros.db"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = "sqlite3:///Database\libros.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///E:\PROGRAMACION PYTHON\API\Database\libros.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 
 @app.route('/')
 def home():
     return '<h1>Hola</h1>'
+
+
+@app.route("/api/libros")
+def getLibros():
+    libros = Libros.query.all()
+    print(libros)
+    return '<h1>Pass</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
--- models.py
+++ models.py
@@ -1,1 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class Libros(db.Model):
+    rowid = db.Column(db.Integer, primary_key=True)
+    Titulo = db.Column(db.String(200), unique=True, nullable=False)
+    Año = db.Column(db.Integer)
+    Autor = db.Column(db.String(200), nullable=False)
