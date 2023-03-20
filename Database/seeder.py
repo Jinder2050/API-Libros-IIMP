f326fc58d79866cdd7416af8436e7dd0833ccf41
Jinder2050 <yindercondori@gmail.com>
2023-03-19 22:34:57 -0500

    Update table

--- Database/seeder.py
+++ Database/seeder.py
@@ -10,7 +10,10 @@ def createDB():
         """CREATE TABLE libros 
         (title TEXT,
         year INT,
-        author TEXT
+        author TEXT,
+        languaje TEXT,
+        status TEXT,
+        url TEXT
         )"""
     )
     conn.commit()
@@ -20,12 +23,22 @@ def addValues():
     conn = sql.connect(DB_PATH)
     cursor = conn.cursor()
     data = [
-        ("INDICADOR DE COMPETITIVIDAD MINERA 2022",2022, "Gonzalo Tamayo - Socio Macroconsult"),
-        ("ESPECIAL PERUMIN 35",2022,"IIMP"),
-        ("COMPENDIO JUEVES MINERO 2022",2022,"IIMP"),
-        ("Minería y Equilibrio Económico",2021,"Claudia Cooper Fort")
+        ("Indicador de competitividad minera",2022, "Gonzalo Tamayo - Socio Macroconsult", 'Español','Publicado','https://iimp.org.pe/asociado/memorias/2022/2022_09_28_Presentacion_PERUMIN_35.pdf'),
+        ("Especial perumin 35",2022,"IIMP", 'Español','Publicado','https://perumin.com/perumin35/especial/ppt/ESPECIAL%20PERUMIN%2035.pdf'),
+        ("Compendio jueves minero 2022",2022,"IIMP",'Español','Publicado','https://iimp.org.pe/jueves_minero/compendio/2022/pdf/Especial.pdf'),
+        ("Minería y Equilibrio Económico",2021,"Claudia Cooper Fort", 'Español','Publicado','https://iimp.org.pe/archivos/publicaciones/LIBRO-MINERIA-Y-EQUILIBRIO-ECONOMICO.pdf'),
+        ('Estudio de proveedores mineros del Peru',2021,'SAMMI - Clúster Minero Andino','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20211124-123006-1fa1.pdf'),
+        ('Estudio: Beneficios de la Minería en el Perú',2021,'Centro para la Competitividad y el Desarrollo','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20210326-023338-2454.pdf'),
+        ('Estudio: Oportunidad de Desarrollo Frente a la Crisis Nacional',2021,'PERUMIN','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20210406-071159-1db4.pdf'),
+        ('Encuesta Minería 2021: Alerta máxima ante el riesgo político',2021,'BNamericas','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20211228-062736-1794.pdf'),
+        ('Mining Survey 2021: High Alert as Political Risk Takes Center Stage',2021,'BNamericas','Inglés','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20211228-063132-113e.pdf'),
+        ('Especial Jueves Mineros Virtuales 2021',2021,'IIMP','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20220309-035638-1387.pdf'),
+        ('Especial Rumbo a PERUMIN',2021,'IIMP','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20220309-035748-24d3.pdf'),
+        ('Minería del Bicentenario',2021,'IIMP','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20220309-035841-bf5.pdf'),
+        ('Especial I Congreso de Competitividad Minera y Sostenibilidad social',2021,'IIMP','Español','Publicado','https://iimp.org.pe/archivos/publicaciones/a621-20220309-040014-630.pdf'),
+        ('Memoria Institucional 2021',2021,'IIMP','Español','Publicado','https://iimp.org.pe/asociado/memorias/2021/')
     ]
-    cursor.executemany("""INSERT INTO libros VALUES (?, ?, ?)""",data)
+    cursor.executemany("""INSERT INTO libros VALUES (?, ?, ?, ?, ?, ?)""",data)
     conn.commit()
     conn.close()
 
